3fdeb89db9afdc121d5baf114116932c5580d30e
radsimu <radu.simionescu@cgm.com>
2016-10-31 18:13:51 +0200

    clean up and set names to attributes

--- maltparser-1.9.0 - sspr/InputEnrichFeatureAdapter/src/AddFeatureColumns.java
+++ maltparser-1.9.0 - sspr/InputEnrichFeatureAdapter/src/AddFeatureColumns.java
@@ -37,69 +37,81 @@ public class AddFeatureColumns {
             String word = columns[1];
             String lemma = columns[2];
             String pos = columns[4];
-            String extraFeats = null;
+            String extraFeats = columns[5].trim();
+            if (extraFeats.equals("_")) extraFeats+="|";
 
-            if (pos.startsWith("Vm")) {
-                List<Annotation> annotations = dictionary.get(lemma);
-                if (annotations != null)
-                    for (Annotation a : annotations) {
-                        if (a.msd.equals("Vmn")) {
-                            extraFeats = featsForVerb(a);
-                            if (extraFeats != null)
-                                break;
+
+            //add verbal feats
+            {
+                String verbalFeats = null;
+
+                if (pos.startsWith("Vm")) {
+                    List<Annotation> annotations = dictionary.get(lemma);
+                    if (annotations != null)
+                        for (Annotation a : annotations) {
+                            if (a.msd.equals("Vmn")) {
+                                verbalFeats = featsForVerb(a);
+                                if (verbalFeats != null)
+                                    break;
+                            }
+                        }
+                } else if (pos.startsWith("Nc")) {
+                    if (!word.endsWith("ele") && !word.endsWith("elor")) {
+                        verbalFeats = verbalFeatsForWord(lemma, "Vmp"); //substantivizare din supin
+                    }
+
+                    if (verbalFeats == null) {
+                        String possibleVerb = null;
+                        if (lemma.endsWith("ție")) {
+                            possibleVerb = lemma.substring(0, lemma.length() - 3); //demonstratie
+                            verbalFeats = verbalFeatsForWord(possibleVerb, "Vmn");
+                            if (verbalFeats == null)
+                                possibleVerb = lemma.substring(0, lemma.length() - 1) + "ona"; //tranzactie ambitie
+                            verbalFeats = verbalFeatsForWord(possibleVerb, "Vmn");
+                        } else if (lemma.endsWith("zie")) {
+                            possibleVerb = lemma.substring(0, lemma.length() - 3) + "da";//explozie
+                            verbalFeats = verbalFeatsForWord(possibleVerb, "Vmn");
+                            if (verbalFeats == null)
+                                possibleVerb = lemma.substring(0, lemma.length() - 3) + "de";//decizie
+                            verbalFeats = verbalFeatsForWord(possibleVerb, "Vmn");
+                        } else if (lemma.endsWith("ie")) {
+                            possibleVerb = lemma.substring(0, lemma.length() - 1); //calarie
+                            verbalFeats = verbalFeatsForWord(possibleVerb, "Vmn");
+                        } else if (lemma.endsWith("ătură")) {
+                            possibleVerb = lemma.substring(0, lemma.length() - 5) + "a"; //aruncatura sapatura
+                            verbalFeats = verbalFeatsForWord(possibleVerb, "Vmn");
+                        } else if (lemma.endsWith("ură")) {
+                            possibleVerb = lemma.substring(0, lemma.length() - 3); //fosnitura, scursura, taratura,
+                            verbalFeats = verbalFeatsForWord(possibleVerb, "Vmp");
+                        } else if (lemma.endsWith("re")) {
+                            possibleVerb = lemma.substring(0, lemma.length() - 2); //cunoastere
+                            verbalFeats = verbalFeatsForWord(possibleVerb, "Vmn");
+                        } else if (lemma.endsWith("iune")) {
+                            possibleVerb = lemma.substring(0, lemma.length() - 3) + "ona"; //actiune, operatiune
+                            verbalFeats = verbalFeatsForWord(possibleVerb, "Vmn");
+                        } else if (lemma.endsWith("eală")) {
+                            possibleVerb = lemma.substring(0, lemma.length() - 4) + "i"; //pacaleala, vrajeala, socoteala, greseala
+                            verbalFeats = verbalFeatsForWord(possibleVerb, "Vmn");
+                        } else if (lemma.endsWith("aj")) {
+                            possibleVerb = lemma.substring(0, lemma.length() - 1); //pilotaj bricolaj
+                            verbalFeats = verbalFeatsForWord(possibleVerb, "Vmn");
+                        } else if (lemma.endsWith("oare")) {
+                            possibleVerb = lemma.substring(0, lemma.length() - 4); //tunsoare
+                            verbalFeats = verbalFeatsForWord(possibleVerb, "Vmp");
                         }
                     }
-            } else if (pos.startsWith("Nc")) {
-                if (!word.endsWith("ele") && !word.endsWith("elor")) {
-                    extraFeats = verbalFeatsForWord(lemma, "Vmp"); //substantivizare din supin
                 }
 
-                if (extraFeats == null) {
-                    String possibleVerb = null;
-                    if (lemma.endsWith("ție")) {
-                        possibleVerb = lemma.substring(0, lemma.length() - 3); //demonstratie
-                        extraFeats = verbalFeatsForWord(possibleVerb, "Vmn");
-                        if (extraFeats == null)
-                            possibleVerb = lemma.substring(0, lemma.length() - 1) + "ona"; //tranzactie ambitie
-                        extraFeats = verbalFeatsForWord(possibleVerb, "Vmn");
-                    } else if (lemma.endsWith("zie")) {
-                        possibleVerb = lemma.substring(0, lemma.length() - 3) + "da";//explozie
-                        extraFeats = verbalFeatsForWord(possibleVerb, "Vmn");
-                        if (extraFeats == null)
-                            possibleVerb = lemma.substring(0, lemma.length() - 3) + "de";//decizie
-                        extraFeats = verbalFeatsForWord(possibleVerb, "Vmn");
-                    } else if (lemma.endsWith("ie")) {
-                        possibleVerb = lemma.substring(0, lemma.length() - 1); //calarie
-                        extraFeats = verbalFeatsForWord(possibleVerb, "Vmn");
-                    } else if (lemma.endsWith("ătură")) {
-                        possibleVerb = lemma.substring(0, lemma.length() - 5) + "a"; //aruncatura sapatura
-                        extraFeats = verbalFeatsForWord(possibleVerb, "Vmn");
-                    } else if (lemma.endsWith("ură")) {
-                        possibleVerb = lemma.substring(0, lemma.length() - 3); //fosnitura, scursura, taratura,
-                        extraFeats = verbalFeatsForWord(possibleVerb, "Vmp");
-                    } else if (lemma.endsWith("re")) {
-                        possibleVerb = lemma.substring(0, lemma.length() - 2); //cunoastere
-                        extraFeats = verbalFeatsForWord(possibleVerb, "Vmn");
-                    } else if (lemma.endsWith("iune")) {
-                        possibleVerb = lemma.substring(0, lemma.length() - 3) + "ona"; //actiune, operatiune
-                        extraFeats = verbalFeatsForWord(possibleVerb, "Vmn");
-                    } else if (lemma.endsWith("eală")) {
-                        possibleVerb = lemma.substring(0, lemma.length() - 4) + "i"; //pacaleala, vrajeala, socoteala
-                        extraFeats = verbalFeatsForWord(possibleVerb, "Vmn");
-                    } else if (lemma.endsWith("aj")) {
-                        possibleVerb = lemma.substring(0, lemma.length() - 1); //pilotaj bricolaj
-                        extraFeats = verbalFeatsForWord(possibleVerb, "Vmn");
-                    } else if (lemma.endsWith("oare")) {
-                        possibleVerb = lemma.substring(0, lemma.length() - 4); //tunsoare
-                        extraFeats = verbalFeatsForWord(possibleVerb, "Vmp");
-                    }
+                if (verbalFeats == null) {
+                    verbalFeats = "is_action=false|transitive=NA";
                 }
+
+                extraFeats += verbalFeats;
             }
 
-            if (extraFeats != null) {
-                columns[5] = extraFeats;
-                //System.out.println(word + "  =>  " + extraFeats);
-            }
+            //add other feats to extraFeats HERE
+
+            columns[5]=extraFeats;
 
             out.write(String.join("\t", columns));
             out.write("\n");
@@ -126,11 +138,11 @@ public class AddFeatureColumns {
         if (a.extra != null) {
             if (a.extra.contains("Stripped"))
                 return null;
-            extraFeats = "action";
+            extraFeats = "is_action=true";
             if (a.extra.contains("intranzitiv")) {
-                extraFeats += "|intranzitiv";
+                extraFeats += "|transitive=false";
             } else if (a.extra.contains("tranzitiv")) {
-                extraFeats += "|tranzitiv";
+                extraFeats += "|transitive=true";
             }
         }
 
--- maltparser-1.9.0 - sspr/InputEnrichFeatureAdapter/src/TrainAndEvalBatch.java
+++ maltparser-1.9.0 - sspr/InputEnrichFeatureAdapter/src/TrainAndEvalBatch.java
@@ -1,5 +1,7 @@
 import maltProcessing.EvaluateMaltModel;
 import org.maltparser.Malt;
+import org.maltparser.MaltParserService;
+import org.maltparser.core.exception.MaltChainedException;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -10,11 +12,14 @@ import java.util.Map;
  * Created by Planetaria on 10/26/2016.
  */
 public class TrainAndEvalBatch {
-    public static void main (String[] args) throws IOException {
+    public static void main (String[] args) throws IOException, MaltChainedException {
         AddFeatureColumns.addFeatures(new File(args[0]), new File("train_corpus_++.txt"));
         AddFeatureColumns.addFeatures(new File(args[1]), new File("test_corpus_++.txt"));
-        Malt.main(new String[]{"-c", "maltmodel", "-f", "train_options.xml", "-i", "train_corpus_++.txt", "-m", "learn", "-ic", "UTF-8"});
-        Malt.main(new String[]{"-c", "maltmodel", "-i", "test_corpus_++.txt", "-o", "test_corpus_parsed.txt", "-m", "parse", "-ic", "UTF-8"});
+        MaltParserService service = new MaltParserService(0);
+        service.runExperiment("-c maltmodel -f train_options.xml -i train_corpus_++.txt -m learn -ic UTF-8");
+        service.runExperiment("-c maltmodel -f train_options.xml -i test_corpus_++.txt -o test_corpus_parsed.txt -m parse -ic UTF-8");
+        //Malt.main(new String[]{"-c", "maltmodel", "-f", "train_options.xml", "-i", "train_corpus_++.txt", "-m", "learn", "-ic", "UTF-8"});
+        //Malt.main(new String[]{"-c", "maltmodel", "-i", "test_corpus_++.txt", "-o", "test_corpus_parsed.txt", "-m", "parse", "-ic", "UTF-8"});
         Map<String, Float> eval = EvaluateMaltModel.Eval(new FileInputStream("test_corpus_++.txt"), new FileInputStream("test_corpus_parsed.txt"));
         System.out.println(eval.toString());
     }
