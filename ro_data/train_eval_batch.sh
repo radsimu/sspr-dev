23ca099eb108216ecf193b752368f19ff7991886
radsimu <radu.simionescu@cgm.com>
2016-11-20 00:02:16 +0200

    adjust script for cross eval

--- ro_data/train_eval_batch.sh
+++ ro_data/train_eval_batch.sh
@@ -2,11 +2,11 @@
 train="${1##*/}"
 test="${2##*/}"
 
-export CLASSPATH="../out/production/InputEnrichFeatureAdapter/":"../out/production/maltparser-1.9.0 - sspr/":"../lib/*"
-java -Xms1300m -Dfile.encoding=utf-8 AddFeatureColumns $1 $train++
-java -Xms1300m -Dfile.encoding=utf-8 AddFeatureColumns $2 $test++
-java -Xms2000m -Dfile.encoding=utf-8 org.maltparser.Malt -c $3 -f train_options.xml -i $train++ -m learn -ic UTF-8
-java -Xms2000m -Dfile.encoding=utf-8 org.maltparser.Malt -c $3 -f train_options.xml -i $test++ -o $test++.parsed -m parse -ic UTF-8
+export CLASSPATH="../out/production/AddConllFeatures/":"../out/production/maltparser-1.9.0 - sspr/":"../lib/*"
+java -Xms1300m -Dfile.encoding=utf-8 AddFeatureColumns $1 $train++ 
+java -Xms1300m -Dfile.encoding=utf-8 AddFeatureColumns $2 $test++ 
+java -Xms1000m -Dfile.encoding=utf-8 org.maltparser.Malt -c $3 -f train_options.xml -i $train++ -m learn -ic UTF-8 
+java -Xms1000m -Dfile.encoding=utf-8 org.maltparser.Malt -c $3 -f train_options.xml -i $test++ -o $test++.parsed -m parse -ic UTF-8 
 ret=$(java -Dfile.encoding=utf-8 maltProcessing.EvaluateMaltModel $test++ $test++.parsed)
-rm $test++ $test++.parsed $train++ $3
+rm -rf $test++ $test++.parsed $train++ $3.mco
 echo $test $ret
